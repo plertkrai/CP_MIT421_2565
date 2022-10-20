b035e096dbb04e95394c6500703a9ee2895c2a26
samPuriwat <puriwat.l@rmutsv.ac.th>
2022-10-20 17:25:27 +0700

    lab 10: Exercise

--- Lab10/exercise_lab10.py
+++ Lab10/exercise_lab10.py
@@ -9,3 +9,15 @@ Group: MIT421
 โดยการรับ Input 
 แม่สูตรคูณจากผู้ใช้มา 1 ตัวเลย
 """
+
+num = int(input("Enter an integer: "))
+
+try:
+    f = open("C:\\Users\Puriw\OneDrive\Desktop\\test3.txt","a")
+    for x in range(1,13):
+        f.write(f'{num} x {x} = {num*x}\n')
+except:
+    print("Cloud not found a file.")
+finally:
+    f.close()
+    print("Done.")
